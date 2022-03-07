ff54f5c39ba24295249c9dd2f726f53335b04585
deveezy <deveezysl@gmail.com>
2022-03-07 18:41:55 +0300

    Tests.

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,24 @@
 #include <functional>
 #include <iostream>
 #include <map>
+#include <memory>
 #include <string>
+#include <thread>
 
-#include "Event.hpp"
+#include "EventManager.hpp"
+#include "events/DayNightEvent.hpp"
+#include "events/ResponseEvent.hpp"
 
 
 int main() {
-  EventType et;
-  EventType::TYPE_ONE & EventType::TYPE_THREE;
+  EventManager em;
+  std::unique_ptr<Event> response_event {new ResponseEvent};
+  std::unique_ptr<Event> day_night_event {new DayNightEvent};
+  em.Bind(EventType::RESPONSE, std::move(response_event));
+  em.Bind(EventType::TYPE_TWO, std::move(day_night_event));
+  std::this_thread::sleep_for(std::chrono::seconds(5));
+  std::cout << "after sleep\n";
+  std::cout.flush();
+  em.Push(EventType::TYPE_TWO);
   return 0;
 }
