294e23b3f7afd5e0693145cdb65cfda2b9bc3d03
deveezy <deveezysl@gmail.com>
2022-03-07 19:05:36 +0300

    test main

--- src/main.cpp
+++ src/main.cpp
@@ -4,41 +4,11 @@
 #include <map>
 #include <string>
 
-#include "EventHandler.hpp"
+#include "Event.hpp"
 
-// class Demo {
-// public:
-//   Demo() = default;
-//   Demo(const Demo &demo) { std::cout << "copy ctor\n"; }
-//   Demo(Demo &&demo) { std::cout << "move ctor\n"; }
-
-//   Demo &operator=(const Demo &demo) {
-//     std::cout << "copy assignment";
-//     return *this;
-//   }
-//   Demo &operator=(Demo &&demo) {
-//     std::cout << "move assignment";
-//     return *this;
-//   }
-// };
-
-// void Pass(Demo _demo) {
-// 	Demo demo = std::move(_demo);
-// }
-
-struct Base {
-  void exec() { std::cout << "base\n"; }
-};
-
-struct Derived : Base {
-  void exec() { std::cout << "der\n"; }
-};
-
-void Pass(Base _base) {
-  _base.exec();
-}
 
 int main() {
-  Pass(Derived {});
+  EventType et;
+  EventType::TYPE_ONE & EventType::TYPE_THREE;
   return 0;
 }
