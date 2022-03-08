de5d4876a1eba06348ccd5be9b31893e248ba721
deveezy <deveezysl@gmail.com>
2022-03-08 18:55:06 +0300

    test exclude

--- src/main.cpp
+++ src/main.cpp
@@ -7,21 +7,24 @@
 #include <memory>
 #include <string>
 #include <thread>
+#include <utility>
 
 #include "EventManager.hpp"
 #include "events/DayNightEvent.hpp"
 #include "events/ResponseEvent.hpp"
-
 
 int main() {
 #if 1
   EventManager em;
   std::unique_ptr<Event> response_event {new ResponseEvent};
   std::unique_ptr<Event> day_night_event {new DayNightEvent};
-  em.Bind(EventType::MOTION, std::move(response_event));
-  em.Bind(EventType::TEMPERATURE, std::move(day_night_event));
-  std::this_thread::sleep_for(std::chrono::seconds(5));
-  std::cout << "Event occurred\n";
+  em.Bind(EventType::MOTION | EventType::TEMPERATURE, std::move(response_event));
+  // em.Bind(EventType::TEMPERATURE, std::move(day_night_event));
+  std::this_thread::sleep_for(std::chrono::seconds(2));
+  em.Push(EventType::TEMPERATURE);
+  std::this_thread::sleep_for(std::chrono::seconds(2));
+  em.Exclude(3, EventType::TEMPERATURE);
+  em.Exclude(3, EventType::MOTION);
   em.Push(EventType::TEMPERATURE);
 #endif
 
