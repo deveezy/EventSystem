68463a64261697c6ca17eac9898ce0a69a9ac4ee
deveezy <deveezysl@gmail.com>
2022-03-08 17:43:24 +0300

    Fake DB added, so implementation has been changed.

--- src/EventManager.cpp
+++ src/EventManager.cpp
@@ -1,7 +1,12 @@
 #include "EventManager.hpp"
 
+#include <stdint.h>
+
+#include <algorithm>
 #include <iostream>
 
+// TODO: fake DB.
+static uint32_t id_counter = 3;
 
 EventManager::EventManager() : events {20} {
   handler = std::async(&EventManager::processEvents, this);
@@ -10,28 +15,39 @@ EventManager::EventManager() : events {20} {
 
 EventManager::~EventManager() = default;
 
-void EventManager::Push(EventType _event_type) { events.TryPush(_event_type); }
+void EventManager::Push(Flags<EventType> _event_type) { events.TryPush(_event_type); }
 
-void EventManager::Bind(EventType _event_type, std::unique_ptr<Event> &&_event) {
-  event_actions.emplace(_event_type, std::move(_event));
+void EventManager::Bind(Flags<EventType> _event_type, std::unique_ptr<Event> &&_event) {
+  _event->SetEventType(_event_type);
+  event_actions.emplace(id_counter, std::move(_event));
+  ++id_counter;
 }
 
-void EventManager::Unbind(EventType _event_type) {
-  
+void EventManager::Exclude(uint32_t _id, Flags<EventType> _event_type) {
+  for (auto &it : event_actions) {
+    if (it.first == _id) {
+      auto existed = it.second->GetEventType();
+      existed &= (~_event_type);
+      if (!existed) event_actions.erase(it.first);
+      else
+        it.second->SetEventType(existed);
+    }
+  }
 }
 
 void EventManager::processEvents() {
   EventType event_type;
   std::cout << "Wait for event...\n";
-  std::cout.flush();
-  events.WaitAndPop(event_type);  // blocked
-  std::cout << "The queue has been filled\n";
+  events.WaitAndPop(event_type);  // block
+  std::cout << "New event just arrived\n";
 
-  for (const auto &element : event_actions) {
-    if (element.first & event_type) { element.second->Execute(); }
-  }
+  const auto comp = [&event_type](
+                        const auto &pair) { return pair.second->GetEventType() & event_type; };
+
+  const auto event_it = std::find_if(event_actions.begin(), event_actions.end(), comp);
+  if (event_it != event_actions.end()) event_it->second->Execute();
 }
 
 void EventManager::initEvents() {
-  // Get values from DB and push to event_actions map.
+  // TODO: Get values from DB and push to event_actions map.
 }
--- src/EventManager.hpp
+++ src/EventManager.hpp
@@ -1,10 +1,7 @@
 #pragma once
 #include <future>
-#include <map>
 #include <memory>
 #include <unordered_map>
-#include <utility>
-#include <vector>
 
 #include "ConcurrentQueue.hpp"
 #include "Event.hpp"
@@ -20,16 +17,17 @@ public:
 
 public:
   /** Push to the queue **/
-  void Push(EventType _event_type);  // Call when the event occurred.
-  void Bind(EventType _event_type, std::unique_ptr<Event> &&_event);  // Call from UI.
-  void Unbind(EventType _event_type);
+  void Push(Flags<EventType> _event_type);  // Call when the event occurred.
+  void Bind(Flags<EventType> _event_type, std::unique_ptr<Event> &&_event);  // Call from UI.
+  void Unbind(uint32_t _id, Flags<EventType>);                               // Call from UI.
+  void Exclude(uint32_t _id, Flags<EventType> _event_type);
 
 private:
   void processEvents();  // queue.pop blocked (producer/consumer)
   void initEvents();
 
 private:
-  std::unordered_multimap<EventType, std::unique_ptr<Event>> event_actions;
+  std::unordered_map<uint32_t, std::unique_ptr<Event>> event_actions;
   ConcurrentQueue<EventType> events;
 
 private:
