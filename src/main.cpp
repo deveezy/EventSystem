30aa59431ccf3f2942d6472c2db5b297f3c3478b
Sergey <80103622+deveezy@users.noreply.github.com>
2022-03-09 13:47:23 +0300

    Demo type dispatch with var args

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,36 @@
 #include <memory>
 #include <string>
 #include <thread>
+#include <type_traits>
 #include <utility>
 
+#include "Event.hpp"
 #include "EventManager.hpp"
 #include "events/DayNightEvent.hpp"
 #include "events/ResponseEvent.hpp"
 
+using ActionDayNight = std::integral_constant<ActionType, ActionType::DAY_NIGNT>;
+using ActionResponse = std::integral_constant<ActionType, ActionType::RESPONSE>;
+
+template <typename... Args>
+std::shared_ptr<Event> GetEvent(ActionDayNight, Args &&..._args) {
+  std::shared_ptr<Event> event;
+  event = std::shared_ptr<DayNightEvent>(new DayNightEvent(std::forward<Args...>(_args...)));
+  return event;
+}
+
+template <typename... Args>
+std::shared_ptr<Event> GetEvent(ActionResponse, Args &&..._args) {
+  std::shared_ptr<Event> event;
+  event = std::shared_ptr<ResponseEvent>(new ResponseEvent(std::forward<Args...>(_args...)));
+  return event;
+}
+
 int main() {
-#if 1
   EventManager em;
+#if 0
   std::shared_ptr<Event> response_event {new ResponseEvent};
-  std::shared_ptr<Event> day_night_event {new DayNightEvent};
+  std::shared_ptr<Event> day_night_event {new DayNightEvent(DayNightEvent::Mode::DAY)};
   em.Bind(EventType::MOTION | EventType::TEMPERATURE, response_event);
   // em.Bind(EventType::TEMPERATURE, day_night_event);
   std::this_thread::sleep_for(std::chrono::seconds(2));
@@ -27,5 +46,18 @@ int main() {
   em.Exclude(3, EventType::MOTION);
   em.Push(EventType::TEMPERATURE);
 #endif
-  return 0;
+
+  // em.CreateEvent(1, DayNightEvent::Mode::DAY);
+  // em.CreateEvent(ActionType::RESPONSE, 10);
+
+  // int a = 10;
+  // int b = 20;
+  // int c = 30;
+
+  const auto type = ActionType::DAY_NIGNT;
+  if (type == ActionType::DAY_NIGNT) {
+    auto ret = GetEvent(ActionDayNight {}, DayNightEvent::Mode::NIGHT);
+  } else if (type == ActionType::RESPONSE) {
+    auto ret = GetEvent(ActionResponse {}, 10);
+  }
 }
