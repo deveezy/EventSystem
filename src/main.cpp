5ca14cc80e83862cc08e3c5598e65124538828f8
deveezy <deveezysl@gmail.com>
2022-03-09 22:07:57 +0300

    side effects

--- src/main.cpp
+++ src/main.cpp
@@ -10,21 +10,20 @@
 #include <type_traits>
 #include <utility>
 
-#include "Event.hpp"
 #include "EventManager.hpp"
 #include "events/DayNightEvent.hpp"
+#include "events/Event.hpp"
 #include "events/ResponseEvent.hpp"
-
 
 int main() {
   EventManager em;
 #if 1
   std::shared_ptr<Event> response_event {new ResponseEvent};
   std::shared_ptr<Event> day_night_event {new DayNightEvent(DayNightEvent::Mode::DAY)};
-  em.Bind(EventType::MOTION, response_event);
-  // em.Bind(EventType::TEMPERATURE, day_night_event);
+  em.Bind(TriggerType::MOTION, response_event);
+  // em.Bind(TriggerType::TEMPERATURE, day_night_event);
   std::this_thread::sleep_for(std::chrono::seconds(2));
   std::this_thread::sleep_for(std::chrono::seconds(2));
-  em.Exclude(3, EventType::MOTION);
+  em.Exclude(3, TriggerType::MOTION);
 #endif
 }
