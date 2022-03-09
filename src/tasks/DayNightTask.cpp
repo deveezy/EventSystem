49bb4684108a7aee6825993c4630234f51df369c
deveezy <deveezysl@gmail.com>
2022-03-09 22:06:40 +0300

    Basic implementation of DayNightTAsk.

---
+++ src/tasks/DayNightTask.cpp
@@ -0,0 +1,19 @@
+#include "DayNightTask.hpp"
+
+#include <iostream>
+
+DayNightTask::DayNightTask(Mode _begin, Mode _end)
+    : Task {TriggerType::DAYNIGHT}
+    , begin {_begin}
+    , end {_end} {}
+
+void DayNightTask::OnBegin() {
+  // TODO: set begin mode to the day_night system from engine.
+  std::cout << "BEGIN MODE WAS SET\n";
+}
+
+void DayNightTask::OnEnd() {
+  // TODO: set end mode to the day_night system from engine.
+  std::cout << "END MODE WAS SET\n";
+  std::cout << "";
+}
